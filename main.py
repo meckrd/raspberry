2548b3137c8a5bec41e924a54001e0a0e51be96b
meckrd <40176223+meckrd@users.noreply.github.com>
2021-01-07 13:42:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import os
+import shutil
+print(os.path.exists("D:\\test.txt"))
+print(os.path.exists("D:\\django\\raspberry\\5.jpg"))
+shutil.copyfile("D:\\django\\raspberry\\5.jpg","D:\\django\\raspberry\\6.jpg")
